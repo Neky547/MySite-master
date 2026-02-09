c8fa307b11e3e486611626d3d260f983813da342
Neky547 <nekenarab55@gmail.com>
2026-02-09 16:58:05 +0100

    Ajout de scripts/Stages

---
+++ script.js
@@ -0,0 +1,7 @@
+//PARCOURS PRO - AFFICHAGE DES DETAILS DES STAGES
+document.getElementById('stageBTS1').addEventListener('click', function() {
+    alert("Détails du stage BTS 1");
+});
+document.getElementById('stageBTS2').addEventListener('click', function() {
+    alert("Détails du stage BTS 2");
+});
