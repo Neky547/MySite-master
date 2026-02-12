8ac4bd9a1eecfb1a68e495843c3eaa6f953bbadb
Neky547 <nekenarab55@gmail.com>
2026-02-12 16:19:03 +0100

    Animation JS

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+//ANIMATIONS
+
+const profileCard = document.getElementById("profile-card");
+const skillsSection = document.getElementById("skills-section");
+const leftTimelineItem = document.getElementById("left-timeline-item");
+const rightTimelineItem = document.getElementById("right-timeline-item");
+
+//Photo de profil
+profileCard.animate(
+  [{ transform: "translateY(70px)" }, { transform: "translateY(0px)" }],
+  { duration: 1000 },
+);
+//Compétences
+skillsSection.animate(
+  [{ transform: "translateX(70px)" }, { transform: "translateX(0px)" }],
+  { duration: 1000 },
+);
+//Parcours scolaire
+leftTimelineItem.animate(
+  [{ transform: "translateX(-200px)" }, { transform: "translateX(0px)" }],
+  { duration: 5000},
+);
+rightTimelineItem.animate(
+  [{ transform: "translateX(200px)" }, { transform: "translateX(0px)" }],
+  { duration: 5000 },
+);
+
 //PARCOURS PRO - AFFICHAGE DES DETAILS DES STAGES
 const modal = new bootstrap.Modal(document.getElementById("stageModal"));
 
@@ -13,7 +40,7 @@ const data = {
       Nous avons participé à la création d'une application de supply chain qui sert à créer et gérer les jeux de tests.
       Dans cette application, nous avons géré la base de données et développé des fonctionnalités pour l'application.
       </p>
-    `
+    `,
   },
   stageBTS2: {
     title: "Stage de 2e année - Développeur Web",
@@ -25,16 +52,14 @@ const data = {
       <p>Nous avons continué à travailler sur l'application de supply chain, en ajoutant de nouvelles fonctionnalités et en améliorant l'interface utilisateur.
       Nous avons également travaillé sur un projet de développement web pour un client, en utilisant WordPress et Elementor pour créer un site web personnalisé.
       </p>
-    `
-  }
+    `,
+  },
 };
 
-document.querySelectorAll("button[id^='stage']").forEach(btn => {
+document.querySelectorAll("button[id^='stage']").forEach((btn) => {
   btn.addEventListener("click", () => {
     document.getElementById("modalTitle").innerHTML = data[btn.id].title;
     document.getElementById("modalBody").innerHTML = data[btn.id].body;
     modal.show();
   });
 });
-
-
